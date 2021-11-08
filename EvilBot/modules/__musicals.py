f4959e0e724fb55481187285d186ce16f2887bc5
luisxgarcia <78333072+luisxgarcia@users.noreply.github.com>
2021-11-08 14:14:07 -0600

    Update __musicals.py
    traducido

--- EvilBot/modules/__musicals.py
+++ EvilBot/modules/__musicals.py
@@ -1,9 +1,9 @@
 __mod_name__ = "◎Music"
 
 __help__ = """
-/video <i>query</i>: download video from youtube
-/deezer <i>query</i>: download from deezer
-/music <i>query</i>: download song from yt servers. (API BASED)
-/lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
-/glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
+/video <i>query</i>: descargar video de youtube
+/deezer <i>query</i>: descargar de deezer
+/music <i>query</i>: descargar cancion de servicios yt. (API BASED)
+/lyrics <i>song name</i> : este complemento busca letras de canciones con el nombre de la canción.
+/glyrics <i> song name </i> : este complemento busca letras de canciones con el nombre de la canción y el artista.
 """
