66920f7d9db6fb4b275d7a8e3654943c853e5a26
luisxgarcia <78333072+luisxgarcia@users.noreply.github.com>
2021-11-08 21:55:51 -0600

    Update __texttospeech.py

--- EvilBot/modules/__texttospeech.py
+++ EvilBot/modules/__texttospeech.py
@@ -18,7 +18,7 @@ async def is_register_admin(chat, user):
             (
                 await tbot(functions.channels.GetParticipantRequest(chat, user))
             ).participant,
-            (types.ChannelParticipantSelf),
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator, types.ChannelParticipantSelf),
         )
     if isinstance(chat, types.InputPeerUser):
         return True
